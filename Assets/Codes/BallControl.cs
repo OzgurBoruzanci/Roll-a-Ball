a62c4787d49b6365f3572583320368631370470f
OzgurBoruzanci <ozgurboruzanc@gmail.com>
2022-09-27 13:14:44 +0300

    Initial commit

---
+++ Assets/Codes/BallControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallControl : MonoBehaviour
+{
+    Rigidbody physics;
+    public int speed;
+    int counter = 0;
+    public int numberOfObjectsToCollect;
+    public Text counterText;
+    public Text gameOverText;
+
+    void Start()
+    {
+        physics = GetComponent<Rigidbody>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 vec = new Vector3(horizontal, 0, vertical);
+        physics.AddForce(vec*speed);
+        //Debug.Log("yatay =" + horizontal + " dikey = " + vertical);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="Cube")
+        {
+            other.gameObject.SetActive(false);
+            counter++;
+            counterText.text = "Counter = " + counter;
+
+            if (counter==numberOfObjectsToCollect)
+            {
+                gameOverText.text = "GAME OVER";
+            }
+
+        }
+    }
+}
---
+++ Assets/Codes/CameraControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public GameObject ball;
+    Vector3 distanceBerween;
+    void Start()
+    {
+        distanceBerween = transform.position - ball.transform.position;
+
+    }
+
+
+    void LateUpdate()
+    {
+        transform.position = ball.transform.position + distanceBerween;
+    }
+}
